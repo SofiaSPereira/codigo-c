b6e2be5ee20276231b3afa7af03cdbae1f60b9a4
SofiaSPereira <sofia.silvapereira@hotmail.com>
2026-09-21 19:20:03 -0300

    AULA 2- Imprimindo informações com função

---
+++ ImprimindoFuncao.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// caractere de escape para saltaar uma linha -> |n
+int main () {
+	
+	//printf e uma funcao de saida
+	printf("ola, seja bem vindo\n");
+	
+	printf("\nvalor retornada: %d", printf("bom dia"));
+	
+	return 0;
+}
